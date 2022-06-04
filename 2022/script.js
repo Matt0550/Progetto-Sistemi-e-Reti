4464715b30c865caf84d4904f5ff8e3c37ab201d
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2022-06-04 14:22:23 +0200

    [+] Edited

---
+++ 2022/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper(".swiper", {
+        autoHeight: false,
+        autoplay: false,
+        speed: 500,
+        direction: "horizontal",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar"
+        },
+        loop: false,
+        effect: "slide",
+        spaceBetween: 30,
+        on: {
+            init: function () {
+                $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+                $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
+            },
+            slideChangeTransitionStart: function () {
+                $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+                $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
+            },
+            // On last slide swipe, go to first slide
+            transitionEnd: function () {
+                if (this.realIndex === 0) {
+                    // Redirect to next page
+                    window.location.href = "../";
+                }
+            }
+        }
+    });
+    mySwiper.slideTo(1);
+    $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
+        mySwiper.slideTo($(this).index());
+        $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+        $(this).addClass("active");
+    });
+
+});
--- script.js
+++ script.js
@@ -23,6 +23,13 @@ $(document).ready(function () {
       slideChangeTransitionStart: function () {
         $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
         $(".swiper-pagination-custom .swiper-pagination-switch").eq(mySwiper.realIndex).addClass("active");
+      },
+      // On last slide swipe, go to first slide
+      transitionEnd: function () {
+        if (this.realIndex === this.slides.length - 1) {
+          // Redirect to next page
+          window.location.href = "./2022/";
+        }
       }
     }
   });
@@ -31,8 +38,6 @@ $(document).ready(function () {
     $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
     $(this).addClass("active");
   });
-
-  
 });
 
 var slideshows = document.querySelectorAll('[data-component="slideshow"]');
@@ -42,7 +47,7 @@ function initSlideShow(slideshow) {
 
 	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
 
-	var index = 0, time = 5000;
+	var index = 0, time = 4000;
 	slides[index].classList.add('active');
 
 	setInterval( () => {
